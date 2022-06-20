89059e5ebc88ade48f96a5ecf3a3044872714fe3
Pooja_K <kanadepooja789@gmail.com>
2022-06-20 08:04:16 +0530

    complete till about.js but some error in movi_id

---
+++ public/js/about.js
@@ -0,0 +1,30 @@
+let movie_id = location.pathname;
+
+
+console.log( movie_id);
+// fetching movie details 
+fetch('${movie_detail_http}${movie_id}?' + new URLSearchParams({
+    api_key: api_key
+}))
+.then(res => res.json())
+.then(data => {
+
+    // console.log(data);
+    setupMovieInfo(data);
+})
+
+const setupMovieInfo = (data) => {
+    const movieName = document.querySelector('.movie-name');
+    const genres = document.querySelector('.genres');
+    const des = document.querySelector('.des');
+    const title = document.querySelector('title');
+    const backdrop = document.querySelector('.movie-info');
+
+    title.innerHTML = movieName.innerHTML = data.title;
+
+    genres.innerHTML = `${data.release_data.split('-')[0]}`;
+    for(let i=0; i< data.genres.length; i++){
+        genres.innerHTML += data.genres[i].name;
+    }
+    
+}
